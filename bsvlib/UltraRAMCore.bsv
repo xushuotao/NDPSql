a4e23007607136109859e2685c7d4e1b6903df0a
Shuotao Xu <shuotao@mit.edu>
2019-11-01 17:13:33 -0400

    enabled UltraRAMCore

---
+++ bsvlib/UltraRAMCore.bsv
@@ -0,0 +1,110 @@
+package UltraRAMCore;
+import BRAMCore::*;
+import List::*;
+
+
+interface UltraRAM_PORT#(type addr, type data);
+   method Action put(Bool write, addr address, data datain);
+   method data read();
+endinterface : UltraRAM_PORT
+
+interface UltraRAM_DUAL_PORT#(type addr, type data);
+   interface UltraRAM_PORT#(addr, data) a;
+   interface UltraRAM_PORT#(addr, data) b;
+endinterface
+
+
+module mkUltraRAMCore2#(Integer pipeline_depth)(UltraRAM_DUAL_PORT#(addr, data)) provisos(Bits#(addr, addr_width),
+                                                                                         Bits#(data, data_width));
+   let m_ = ?;
+   if (genVerilog()) begin
+      m_ <- mkUltraRAMCore2BVI(pipeline_depth);
+   end   
+   else begin
+      m_ <- mkUltraRAMCore2SIM(pipeline_depth);
+   end
+   
+   return m_;
+endmodule
+
+
+module mkUltraRAMCore2SIM#(Integer pipeline_depth)(UltraRAM_DUAL_PORT#(addr, data)) provisos(Bits#(addr, addr_width),
+                                                                                             Bits#(data, data_width));
+   
+   BRAM_DUAL_PORT#(addr, data) ram <- mkBRAMCore2(valueOf(TExp#(addr_width)), False);
+   
+   List#(Reg#(data)) delayRegA <- replicateM(pipeline_depth, mkRegU);
+
+   rule moveA;
+      delayRegA[0] <= ram.a.read();
+      for (Integer i = 0; i < pipeline_depth - 1; i = i + 1) begin
+         delayRegA[i+1] <= delayRegA[i];
+      end
+   endrule
+   
+   List#(Reg#(data)) delayRegB <- replicateM(pipeline_depth, mkRegU);
+
+   rule moveB;
+      delayRegB[0] <= ram.b.read();
+      for (Integer i = 0; i < pipeline_depth - 1; i = i + 1) begin
+         delayRegB[i+1] <= delayRegB[i];
+      end
+   endrule
+
+   
+   interface UltraRAM_PORT a;
+      method Action put(Bool write, addr address, data datain);
+         ram.a.put(write, address, datain);
+      endmethod
+      method data read();
+         return last(delayRegA)._read;
+      endmethod
+   endinterface   
+   interface UltraRAM_PORT b;
+      method Action put(Bool write, addr address, data datain);
+         ram.b.put(write, address, datain);
+      endmethod
+      method data read();
+         return last(delayRegB)._read;
+      endmethod
+   endinterface
+endmodule
+
+
+import "BVI" UltraRAM =
+module mkUltraRAMCore2BVI#(Integer pipeline_depth)(UltraRAM_DUAL_PORT#(addr, data)) provisos(Bits#(addr, addr_width),
+                                                                                             Bits#(data, data_width));
+   default_clock clk(clk);
+   default_reset rst(rst);
+   
+   parameter AWIDTH = valueOf(addr_width);
+   parameter DWIDTH = valueOf(data_width);
+   parameter NBPIPE = pipeline_depth;
+   
+   interface UltraRAM_PORT a;
+      method       put(wea, addra, dina) enable(mem_ena) clocked_by (clk) reset_by(no_reset);
+      method douta read clocked_by (clk) reset_by(no_reset);
+   endinterface
+   
+   schedule (a.put ) CF (a.read);
+   schedule (a.read) CF (a.read);
+   schedule (a.put ) C (a.put);
+   
+   interface UltraRAM_PORT b;
+      method       put(web, addrb, dinb) enable(mem_enb) clocked_by (clk) reset_by(no_reset);
+      method doutb read clocked_by (clk) reset_by(no_reset);
+   endinterface
+
+   schedule (b.put ) CF (b.read);
+   schedule (b.read) CF (b.read);
+   schedule (b.put ) C (b.put);
+
+   schedule (a.put ) CF (b.put);
+   schedule (a.put) CF (b.read);
+   schedule (a.read ) CF (b.put);
+   schedule (a.read ) CF (b.read);
+
+
+endmodule
+
+endpackage: UltraRAMCore
---
+++ verilog/UltraRAM.v
@@ -0,0 +1,117 @@
+//  Xilinx UltraRAM True Dual Port Mode.  This code implements
+//  a parameterizable UltraRAM block with write/read on both ports in
+//  No change behavior on both the ports . The behavior of this RAM is
+//  when data is written, the output of RAM is unchanged w.r.t each port.
+//  Only when write is inactive data corresponding to the address is
+//  presented on the output port.
+//
+module UltraRAM #(
+                  parameter AWIDTH = 12,  // Address Width
+                  parameter DWIDTH = 72,  // Data Width
+                  parameter NBPIPE = 3    // Number of pipeline Registers
+                  ) (
+                     input                   clk, // Clock
+                                           // Port A
+                     input                   rst, // Reset
+                     input                   wea, // Write Enable
+                                           // input                   regcea, // Output Register Enable
+                     input                   mem_ena, // Memory Enable
+                     input [DWIDTH-1:0]      dina, // Data Input
+                     input [AWIDTH-1:0]      addra, // Address Input
+                     output [DWIDTH-1:0] douta,// Data Output
+
+                                           // Port B
+                     // input                   rstb, // Reset
+                     input                   web, // Write Enable
+                                           // input                   regceb, // Output Register Enable
+                     input                   mem_enb, // Memory Enable
+                     input [DWIDTH-1:0]      dinb, // Data Input
+                     input [AWIDTH-1:0]      addrb, // Address Input
+                     output [DWIDTH-1:0] doutb // Data Output
+                     );
+
+   (* ram_style = "ultra" *)
+   reg [DWIDTH-1:0]                                                mem[(1<<AWIDTH)-1:0];        // Memory Declaration
+
+   reg [DWIDTH-1:0]                                                memrega;
+   reg [DWIDTH-1:0]                                                mem_pipe_rega[NBPIPE-1:0];    // Pipelines for memory
+   reg                                                             mem_en_pipe_rega[NBPIPE:0];   // Pipelines for memory enable
+
+   reg [DWIDTH-1:0]                                                memregb;
+   reg [DWIDTH-1:0]                                                mem_pipe_regb[NBPIPE-1:0];    // Pipelines for memory
+   reg                                                             mem_en_pipe_regb[NBPIPE:0];   // Pipelines for memory enable
+   assign douta = mem_pipe_rega[NBPIPE-1];
+   assign doutb = mem_pipe_regb[NBPIPE-1];
+
+   integer                                                         i;
+
+   // RAM : Read has one latency, Write has one latency as well.
+   always @ (posedge clk)
+     begin
+        if(mem_ena)
+          begin
+             if(wea)
+               mem[addra] <= dina;
+             else
+               memrega <= mem[addra];
+          end
+     end
+   // The enable of the RAM goes through a pipeline to produce a
+   // series of pipelined enable signals required to control the data
+   // pipeline.
+   always @ (posedge clk)
+     begin
+        mem_en_pipe_rega[0] <= mem_ena;
+        for (i=0; i<NBPIPE; i=i+1)
+          mem_en_pipe_rega[i+1] <= mem_en_pipe_rega[i];
+     end
+
+   // RAM output data goes through a pipeline.
+   always @ (posedge clk)
+     begin
+        if (mem_en_pipe_rega[0])
+          mem_pipe_rega[0] <= memrega;
+     end
+
+   always @ (posedge clk)
+     begin
+        for (i = 0; i < NBPIPE-1; i = i+1)
+          if (mem_en_pipe_rega[i+1])
+            mem_pipe_rega[i+1] <= mem_pipe_rega[i];
+     end
+
+   // RAM : Read has one latency, Write has one latency as well.
+   always @ (posedge clk)
+     begin
+        if(mem_enb)
+          begin
+             if(web)
+               mem[addrb] <= dinb;
+             else
+               memregb <= mem[addrb];
+          end
+     end
+   // The enable of the RAM goes through a pipeline to produce a
+   // series of pipelined enable signals required to control the data
+   // pipeline.
+   always @ (posedge clk)
+     begin
+        mem_en_pipe_regb[0] <= mem_enb;
+        for (i=0; i<NBPIPE; i=i+1)
+          mem_en_pipe_regb[i+1] <= mem_en_pipe_regb[i];
+     end
+
+   // RAM output data goes through a pipeline.
+   always @ (posedge clk)
+     begin
+        if (mem_en_pipe_regb[0])
+          mem_pipe_regb[0] <= memregb;
+     end
+
+   always @ (posedge clk)
+     begin
+        for (i = 0; i < NBPIPE-1; i = i+1)
+          if (mem_en_pipe_regb[i+1])
+            mem_pipe_regb[i+1] <= mem_pipe_regb[i];
+     end
+endmodule
